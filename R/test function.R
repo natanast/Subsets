e6a276a8a02a307b4c7e329631b310388a6660b1
NatAnastas <anastasiadounat@gmail.com>
2024-03-28 14:09:45 +0200

    #8

--- R/test function.R
+++ R/test function.R
@@ -12,25 +12,25 @@ library(dplyr)
 
 
 #function-----------------------------------------
-   carmilla <- function(
+carmilla <- function(
         
-        q, IGHV.subgroup = FALSE, vgene = FALSE, jgene = FALSE, 
-        CDR3.offset.x = NULL, CDR3.offset.y = NULL, 
-        CDR3.offset.a = "[A-Z]+", CDR3.offset.b = "[A-Z]+",
-        CDR3.offset.Subset.x = NULL, CDR3.offset.Subset.y = NULL,
-        subgroup = c("[A-Z]+[0-9]", "[A-Z]+[0-9]", "[A-Z]+[0-9]"),
-        ighj.filter = "[A-Z]+", CDR3.IMGT.length.filter = NULL, 
-        CDR3.offset.a.true.filter = FALSE, CDR3.offset.b.true.filter = FALSE
-        
-      ) {
+    q, IGHV.subgroup = FALSE, vgene = FALSE, jgene = FALSE, 
+    CDR3.offset.x = NULL, CDR3.offset.y = NULL, 
+    CDR3.offset.a = "[A-Z]+", CDR3.offset.b = "[A-Z]+",
+    CDR3.offset.Subset.x = NULL, CDR3.offset.Subset.y = NULL,
+    subgroup = c("[A-Z]+[0-9]", "[A-Z]+[0-9]", "[A-Z]+[0-9]"),
+    ighj.filter = "[A-Z]+", CDR3.IMGT.length.filter = NULL, 
+    CDR3.offset.a.true.filter = FALSE, CDR3.offset.b.true.filter = FALSE
+    
+) {
     
     
-        q1 <- q |>
-            filter(
-                `V-DOMAIN Functionality` == "productive" |
-                `V-DOMAIN Functionality`== "productive (see comment)",
-                `CDR3-IMGT length` != "X"
-              ) 
+    q1 <- q |>
+        filter(
+            `V-DOMAIN Functionality` == "productive" |
+            `V-DOMAIN Functionality`== "productive (see comment)",
+            `CDR3-IMGT length` != "X"
+        ) 
     
     
     if(IGHV.subgroup) {
@@ -86,7 +86,7 @@ library(dplyr)
         q1 <- q1 |> mutate(
                 `CDR3.offset.Subset` = substr(
                 `AA JUNCTION`, CDR3.offset.Subset.x, CDR3.offset.Subset.y)
-             )
+               )
         
     }
     
@@ -132,10 +132,11 @@ library(dplyr)
 #---------------------Subsets------------------------------
 
 #load path-----------------------------------------
-fls    = "C:/Users/natan/Υπολογιστής/Carmilla 2.0/Samples"
+fls    = "C:/Users/nanastasiadou/Desktop/Carmilla 2.0/Subsets_1/Samples/"
 
 fls    = fls |> 
     list.files(full.names = TRUE) 
+
 
 
 
@@ -146,10 +147,10 @@ for (file in fls) {
     summary_file <- file.path(file, "1_Summary.txt")
     df <- fread(summary_file, sep = "\t", fill = TRUE, select = 1:33)
     
-
+    
     
     # ---------------- subset 1-99---------------------
-
+    
     data <- carmilla(df, IGHV.subgroup = TRUE, vgene = FALSE, jgene = TRUE,
                      CDR3.offset.x = 3,   CDR3.offset.y = 9, 
                      CDR3.offset.a = "QWL", CDR3.offset.b = "",
@@ -162,21 +163,50 @@ for (file in fls) {
     
     
     df1 <- data |>
-        mutate(`is.Subset1` = ifelse(
-               `CDR3-IMGT length` == 13 &
-               `CDR3.offset.a.true` == "QWL",
-                TRUE,
-                FALSE)) |>
-        mutate(`is.Subset99` = ifelse(
-               `CDR3-IMGT length` == 14 &
-               `CDR3.offset.a.true` == "QWL", 
-                TRUE,
-                FALSE))
+              mutate(`is.Subset1` = ifelse(
+                     `CDR3-IMGT length` == 13 &
+                     `CDR3.offset.a.true` == "QWL",
+                      TRUE,
+                      FALSE)) |>
+              mutate(`is.Subset99` = ifelse(
+                     `CDR3-IMGT length` == 14 &
+                     `CDR3.offset.a.true` == "QWL", 
+                      TRUE,
+                      FALSE))
     
     
     
+    #Subset1
+    Subset_1 <- df1 |>
+        filter(is.Subset1 == "TRUE") |>
+        select(1:(ncol(df1) - 7))
     
-
+    
+    
+    #Subset99
+    Subset99 <- df1 |>
+        filter(is.Subset99 == "TRUE") |>
+        select(1:(ncol(df1)-7))  
+    
+    
+    #Satellite
+    Subset1_99_Sat <- df1 |>
+        filter(is.Subset1 == "FALSE",  is.Subset99 == "FALSE") |>
+        select(1:(ncol(df1) - 7))
+    
+    
+    #save file
+    df_list <- list(Subset1 = Subset_1, Subset99 = Subset99,
+                    Satellite = Subset1_99_Sat)
+    
+    # Specify the file path
+    excel_file <- paste0(file, "/Subset_1_99_f.xlsx")
+    
+    # Write the list of data frames to an Excel file with different sheets
+    writexl::write_xlsx(df_list, path = excel_file)
+    
+    
+    
     
     
     
@@ -192,20 +222,53 @@ for (file in fls) {
     
     
     df1 <- data |>
-            mutate(`is.Subset2` = ifelse(
-                   `CDR3-IMGT length` == 9 &
-                  (`CDR3.offset.a.true` == "D" | 
-                   `CDR3.offset.b.true` == "E") &
-                   `IGHV.gene` == "IGHV3-21",
-                    TRUE,
-                    FALSE)) |>
-            mutate(`is.Subset169` = ifelse(
-                   `CDR3-IMGT length` == 9 &
-                  (`CDR3.offset.a.true` == "D" | 
-                   `CDR3.offset.b.true` == "E") &
-                   `IGHV.gene` == "IGHV3-48",
-                    TRUE,
-                    FALSE))
+             mutate(`is.Subset2` = ifelse(
+                    `CDR3-IMGT length` == 9 &
+                   (`CDR3.offset.Subset` == "D" | 
+                    `CDR3.offset.Subset` == "E") &
+                    `IGHV.gene` == "IGHV3-21",
+                     TRUE,
+                     FALSE)) |>
+             mutate(`is.Subset169` = ifelse(
+                    `CDR3-IMGT length` == 9 &
+                   (`CDR3.offset.Subset` == "D" | 
+                    `CDR3.offset.Subset` == "E") &
+                    `IGHV.gene` == "IGHV3-48",
+                     TRUE,
+                     FALSE))
+    
+    
+    
+    #Subset2
+    Subset_2 <- df1 |>
+        filter(`is.Subset2` == "TRUE") |>
+        select(1:(ncol(df1) - 9))
+    
+    
+    #Subset169
+    Subset169 <- df1 |>
+        filter(`is.Subset169` == "TRUE") |>
+        select(1:(ncol(df1) - 9))
+    
+    
+    
+    #Satellite
+    Subset_2_169_Sat <- df1 |>
+        filter(`is.Subset2` == "FALSE", `is.Subset169` == "FALSE") |>
+        select(1:(ncol(df1) - 8))
+    
+    
+    #save file
+    df_list <- list(Subset2 = Subset_2, Subset169 = Subset169, 
+                    Satellite = Subset_2_169_Sat)
+    
+    # Specify the file path
+    excel_file <- paste0(file, "/Subset_2_169_without_J_f.xlsx")
+    
+    # Write the list of data frames to an Excel file with different sheets
+    writexl::write_xlsx(df_list, path = excel_file)
+    
+    
     
     
     
@@ -216,7 +279,7 @@ for (file in fls) {
     data <- carmilla(df, IGHV.subgroup = TRUE, vgene = TRUE, jgene = TRUE,
                      CDR3.offset.x = 5,   CDR3.offset.y = 14, 
                      CDR3.offset.a = "YSSSWY", CDR3.offset.b = "",
-                     CDR3.offset.Subset.x = 5, CDR3.offset.Subset.y = 14,
+                     CDR3.offset.Subset.x = 7, CDR3.offset.Subset.y = 12,
                      subgroup = c("IGHV2", "IGHV4", "IGHV6"), ighj.filter = "IGHJ5",
                      CDR3.IMGT.length.filter = 16:21, 
                      CDR3.offset.a.true.filter = TRUE, 
@@ -224,30 +287,56 @@ for (file in fls) {
     
     
     
-    df1 <- df |>
-        filter(`V-DOMAIN Functionality` == "productive" |
-               `V-DOMAIN Functionality` == "productive (see comment)",
-               `CDR3-IMGT length` != "X") |>
-        mutate(`IGHV.subgroup` = substr(`V-GENE and allele`, 8, 12),
-               `IGHV.gene` = substr(`V-GENE and allele`, 8, 15),
-               `IGHJ.gene` = substr(`J-GENE and allele`, 8, 12),
-               `CDR3.offset` = substr(`AA JUNCTION`, 5, 14),
-               `CDR3.offset.true` = grepl("YSSSWY", `CDR3.offset`),
-               `CDR3.offset.Subset8` = substr(`AA JUNCTION`, 7, 12)) |>
-        filter(`IGHV.subgroup` %in% c("IGHV2", "IGHV4", "IGHV6"),
-               `IGHJ.gene` == "IGHJ5", 
-               `CDR3-IMGT length` %in% 16:21,
-               `CDR3.offset.true` == "TRUE") |>
+    df1 <- data |>
         mutate(`is.Subset.8` = ifelse(
                `CDR3-IMGT length` == 19 &
-               `CDR3.offset.Subset8` == "YSSSWY",
+               `CDR3.offset.Subset` == "YSSSWY",
                 TRUE,
                 FALSE)) |>
         mutate(`is.Subset8B` = ifelse(
                `CDR3-IMGT length` == 18 &
-               `CDR3.offset.Subset8` == "YSSSWY",
+               `CDR3.offset.Subset` == "YSSSWY",
                 TRUE,
                 FALSE))
+    
+    
+    #Subset_8
+    Subset_8 <- df1 |>
+        filter(`is.Subset.8` == "TRUE") |>
+        select(1:(ncol(df1) - 8))
+    
+    
+    #Subset_8B
+    Subset_8B <- df1 |>
+        filter(`is.Subset8B` == "TRUE") |>
+        select(1:(ncol(df1) - 8))
+    
+    
+    
+    #Satellite
+    
+    Subset_8_8B_Sat <- df1 |>
+        filter(`is.Subset.8` == "FALSE",
+               `is.Subset8B` == "FALSE") |>
+        select(1:(ncol(df1) - 7))
+    
+    
+    
+    #save file
+    df_list <- list(Subset_8 = Subset_8, Subset_8B = Subset_8B,
+                    Satellite = Subset_8_8B_Sat)
+    
+    # Specify the file path
+    excel_file <- paste0(file, "/Subset_8_8Β_f.xlsx")
+    
+    # Write the list of data frames to an Excel file with different sheets
+    writexl::write_xlsx(df_list, path = excel_file)
+    
+    
+    
+    
+    
+    
     
     
     
